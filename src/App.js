7a92624f595989f8e47b7214d89672b5c936a4ba
Protik111 <rafiurprotik111@gmail.com>
2022-04-13 13:17:01 +0600

    sorting searching added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TableData from './components/TableData';
+
+function App() {
+  return (
+    <div>
+      <TableData></TableData>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TableData.js
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import BootstrapTable from 'react-bootstrap-table-next';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Box } from "@mui/system";
+import LinearProgress from '@mui/material/LinearProgress';
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.css';
+import filterFactory, { textFilter } from 'react-bootstrap-table2-filter';
+
+const TableData = () => {
+    const [tableHeader, setTableHeader] = useState([])
+    const [dataTable, setDataTable] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost/api/list.php')
+            .then(result => {
+                setTableHeader(result.data.data.headers[0])
+                setDataTable(result.data.data.rows)
+            })
+    }, [])
+
+    if (tableHeader.length <= 0) {
+        return (
+            <Box mt={20} sx={{ width: '100%' }}>
+                <LinearProgress />
+            </Box>
+        )
+    }
+
+    const columns = [
+        { dataField: 'id', text: !tableHeader.id.title ? '' : tableHeader.id.title, sort: tableHeader.id.sortable, filter: tableHeader.id.searchable === true ? textFilter() : false },
+        { dataField: 'name', text: !tableHeader.name.title ? '' : tableHeader.name.title, sort: tableHeader.name.sortable, filter: tableHeader.name.searchable === true ? textFilter() : false },
+        { dataField: 'message', text: tableHeader.message.title, sort: tableHeader.message.sortable, filter: tableHeader.message.searchable === true ? textFilter() : false },
+        { dataField: 'created_at', text: !tableHeader.created_at.title ? '' : tableHeader.created_at.title, sort: tableHeader.created_at.sortable, filter: tableHeader.created_at.searchable === true ? textFilter() : false }
+    ]
+
+    console.log('data', dataTable, tableHeader);
+
+    return (
+        <div>
+            <BootstrapTable
+                bootstrap4
+                keyField='id'
+                columns={columns}
+                data={dataTable}
+                filter={filterFactory()}
+            ></BootstrapTable>
+            {/* <table className="table table-bordered">
+                <thead className="thead-light">
+                    {
+                        tableHeader.map(header => <tr key={Math.random()}>
+                            <th scope="col">{header.id.title}</th>
+                            <th scope="col">{header.name.title}</th>
+                            <th scope="col">{header.message.title}</th>
+                            <th scope="col">{header.created_at.title}</th>
+                        </tr>)
+                    }
+                </thead>
+                <tbody>
+                    {
+                        dataTable.map(order =>
+                            <tr key={order.id}>
+                                <th scope="row">{order.id}</th>
+                                <td>{order.name}</td>
+                                <td>{order.message}</td>
+                                <td>{order.created_at}</td>
+                            </tr>)
+                    }
+                </tbody>
+            </table> */}
+        </div>
+    );
+};
+
+export default TableData;
