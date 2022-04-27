f2706bc5dabce6c90dbfae3045aefec08a44205b
Protik111 <rafiurprotik111@gmail.com>
2022-04-27 11:18:19 +0600

    date and day functionality added lesson 37

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from './Navbar';
 import TableData from './TableData';
+import Time from './Time37/Time';
 
 const Home = () => {
     return (
         <div>
             <Navbar></Navbar>
-            <TableData></TableData>
+            {/* <TableData></TableData> */}
+            <Time></Time>
         </div>
     );
 };
